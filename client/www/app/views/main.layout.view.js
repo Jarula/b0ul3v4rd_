52d73bc1bfd98fa175cbbd0c5e74507f3963c5aa
Matías M. Méndez <matimendez88@gmail.com>
2015-12-10 00:57:04 -0300

    main layout view changes

--- client/www/app/views/main.layout.view.js
+++ client/www/app/views/main.layout.view.js
@@ -9,27 +9,53 @@ App.module('Boulevard.Views', function (Views, App, Backbone, Marionette, $, _) 
             'promosRegion': '[data-js="promos"]'
         },
 
-        events: {},
-
         onShow: function(options) {
             var that = this,
                 headerView,
-                promo,
                 promosCollection,
                 promosCollectionView,
-                totalView;
+                collectionCached;
 
             headerView = new Views.Header();
             this.headerRegion.show(headerView);
 
-            promo = new App.Boulevard.Models.Promo();
-            promosCollection = new App.Boulevard.Collections.People(promo);
-
-            promosCollectionView = new Views.Promos({
-                collection: promosCollection
+            promosCollection = new App.Boulevard.Collections.Promos();
+            promosCollection.fetch({
+                cache: true,
+                expires: 600000,
+                'success': function(collection, response, options){
+                    collection.trigger('fetched');
+                    collectionCached = collection.clone();
+                }
             });
-            this.promosRegion.show(promosCollectionView);
-        }
-    });
 
+            promosCollection.on('fetched', function() {
+                promosCollectionView = new Views.Promos({
+                    collection: promosCollection
+                });
+                that.promosRegion.show(promosCollectionView);
+            });
+
+            App.Events.on('showPromotion', function(modelId) {
+                that.showPromotion(modelId, promosCollection);
+            });
+
+            App.Events.on('showPromotionsList', function() {
+                that.showPromotionsList(promosCollection, collectionCached);
+            });
+        },
+
+        showPromotionsList: function(promosCollection, collectionCached) {
+            promosCollection.reset(collectionCached.models);
+        },
+
+        showPromotion: function(modelId, promosCollection) {
+            promosCollection.reset(promosCollection.filter(function(model) {
+                return model.get('id') === modelId;
+            }));
+
+            App.Events.trigger('Header:Promo');
+        }
+
+    });
 });
