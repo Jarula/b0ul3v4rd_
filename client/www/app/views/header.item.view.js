b12d79e456f17b1977da75a55dd47a1d2e9d8774
Matías M. Méndez <matimendez88@gmail.com>
2015-12-10 01:00:43 -0300

    header view added

---
+++ client/www/app/views/header.item.view.js
@@ -0,0 +1,46 @@
+// 'use strict';
+
+App.module('Boulevard.Views', function (Views, App, Backbone, Marionette, $, _) {    
+    
+    Views.Header = Marionette.ItemView.extend({
+
+        template: __templates.boulevard.header,
+
+        ui: {
+            menu: '.button-collapse',
+            back: '.button-arrow'
+        },
+
+        events: {
+            'click @ui.back': 'showPromotionsList'
+        },
+
+        onRender: function() {
+            var that = this;
+
+            // arreglar esto para no tener que usar un timeout
+            setTimeout(function(){
+                that.ui.menu.sideNav();
+            }, 100);
+
+            App.Events.on('Header:Promo', function() {
+                that.showBackButton();
+            });
+        },
+
+        showPromotionsList: function() {
+            App.Events.trigger('showPromotionsList');
+        },
+
+        showBackButton: function() {
+            this.ui.menu.hide();
+            this.ui.back.show();
+        },
+
+        showMenuButton: function() {
+            this.ui.menu.show();
+            this.ui.back.hide();
+        }
+
+    });
+});
